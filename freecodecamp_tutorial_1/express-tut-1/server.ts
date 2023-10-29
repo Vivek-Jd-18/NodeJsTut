ac943e9da5202e95c8c9e73bfbb7c4947c8c73b0
Vivek-Jd-18 <sergiokunaguero0009@gmail.com>
2023-10-29 17:54:33 +0530

    completed : fcc

--- freecodecamp_tutorial_1/express-tut-1/server.ts
+++ freecodecamp_tutorial_1/express-tut-1/server.ts
@@ -7,7 +7,18 @@ app.use(express.static("./public"))
 app.use(express.urlencoded({extended:false}))
 
 app.get('/node-api/players',(req:any,res:any)=>{
-    res.status(200).json({success:true,data:players})
+    res.status(200).json({success:true,data:players});
+})
+
+
+app.post('/players',(req:any,res:any)=>{
+    const {name} = req.body;
+    console.log(name,req.body);
+    
+    if(!name){
+        return res.status(400).json({success:false,msg:"Please Provide name"})
+    }
+    res.status(201).json({success:true,data:[...players,name]})
 })
 
 app.post("/login",(req:any,res:any)=>{
